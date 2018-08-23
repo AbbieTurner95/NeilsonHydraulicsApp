ad8989ab4d5a2e6e7d2a6953cddad4a49d578dd0
abbieturner <abbieturner95@hotmail.com>
2018-08-23 11:36:28 +0100

    added qr code

---
+++ app/src/main/java/com/example/abbieturner/neilsonsapp/UI/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.abbieturner.neilsonsapp.UI;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.abbieturner.neilsonsapp.R;
+import com.example.abbieturner.neilsonsapp.Utils.Utils;
+import com.fxn.pix.Pix;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.qr_button)
+    Button qr_button;
+
+    public static final int IMAGE_CODE=100;
+    public static final String IMAGE_URL="image_url";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.facebook)
+    public void startFacebook(){
+        startActivity(Utils.getOpenFacebookIntent(this));
+    }
+
+    @OnClick(R.id.linkinin)
+    public void startLinkedIn(){
+        Utils.getOpenLinkedInIntent(this);
+    }
+
+    @OnClick(R.id.twitter)
+    public void startTwitter(){
+        startActivity(Utils.getOpenTwitterIntent(this));
+    }
+
+    @OnClick(R.id.take_pic_button)
+    public void fetchImage(){
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.RECORD_AUDIO,Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()){
+                            Pix.start(MainActivity.this,IMAGE_CODE);
+                        }else{
+                            Utils.goToImageSettings(MainActivity.this);
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                })
+                .onSameThread()
+                .check();
+    }
+
+
+    @OnClick(R.id.qr_button)
+    public void startQrScanning(){
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        startActivity(new Intent(getApplicationContext(),QRActivity.class));
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Utils.goToImageSettings(MainActivity.this);
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                })
+                .onSameThread()
+                .check();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_CODE) {
+            ArrayList<String> returnValue = data.getStringArrayListExtra(Pix.IMAGE_RESULTS);
+            Toast.makeText(this, ""+returnValue.get(0), Toast.LENGTH_SHORT).show();
+
+            //start messaging activity
+            Bundle bundle=new Bundle();
+            bundle.putBoolean(QRActivity.IS_BARCODE,false);
+            bundle.putString(IMAGE_URL,returnValue.get(0));
+            startActivity(new Intent(this,SendMessageActivity.class)
+                    .putExtras(bundle));
+        }
+    }
+}
---
+++ app/src/main/java/com/example/abbieturner/neilsonsapp/UI/QRActivity.java
@@ -0,0 +1,103 @@
+package com.example.abbieturner.neilsonsapp.UI;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.abbieturner.neilsonsapp.R;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import info.androidhive.barcode.BarcodeReader;
+
+public class QRActivity extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener {
+
+    private BarcodeReader barcodeReader;
+
+    //Rec Var
+    public static final String TAG=QRActivity.class.getSimpleName();
+    public static final String BARCODE_RESULT="barcode_result";
+    public static final String IS_BARCODE="is_barcode";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+
+        //Hide Action Bar
+        if (getSupportActionBar()!=null){
+            getSupportActionBar().hide();
+        }
+
+        barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_fragment);
+
+    }
+
+    @Override
+    public void onScanned(final Barcode barcode) {
+        Log.e(TAG, "onScanned: " + barcode.displayValue);
+        barcodeReader.playBeep();
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //Toast.makeText(getApplicationContext(), "Barcode: " + barcode.displayValue, Toast.LENGTH_SHORT).show();
+                Bundle bundle=new Bundle();
+                bundle.putString(BARCODE_RESULT, barcode.displayValue);
+                bundle.putBoolean(IS_BARCODE,true);
+                startActivity(new Intent(getApplicationContext(),SendMessageActivity.class)
+                        .putExtras(bundle));
+            }
+        });
+
+    }
+
+    @Override
+    public void onScannedMultiple(List<Barcode> barcodes) {
+        Log.e(TAG, "onScannedMultiple: " + barcodes.size());
+
+        String codes = "";
+        for (Barcode barcode : barcodes) {
+            codes += barcode.displayValue + ", ";
+        }
+        barcodeReader.playBeep();
+        final String finalCodes = codes;
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //Toast.makeText(getApplicationContext(), "Barcodes: " + finalCodes, Toast.LENGTH_SHORT).show();
+                Bundle bundle=new Bundle();
+                bundle.putString(BARCODE_RESULT, finalCodes);
+                startActivity(new Intent(getApplicationContext(),SendMessageActivity.class));
+
+            }
+        });
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+
+    }
+
+    @Override
+    public void onScanError(String errorMessage) {
+
+    }
+
+    @Override
+    public void onCameraPermissionDenied() {
+        Toast.makeText(getApplicationContext(), "Camera permission denied!", Toast.LENGTH_LONG).show();
+        finish();
+    }
+}
